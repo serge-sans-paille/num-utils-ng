6ba140d70aa5b2ceca15a3f501d4f61973730d2f
edern <edern_hotte@hotmail.fr>
2009-08-20 22:59:59 +0200

    Some tests have been added (cf average).

--- tests/testinterval.sh
+++ tests/testinterval.sh
@@ -2,30 +2,47 @@
 
 #This script should gather all the tests for the function interval.
 
-ERROR_NB=0
+ERROR=0
 LAST_ERROR=0
 
-../src/interval ../tests/test3 2>/dev/null
+echo -e "\nTest results for the interval function :\n" > finalmsg
+
+echo "1 2 3 4" 1> data
+
+interval .data 2>/dev/null
 LAST_ERROR=$?
 if [ "$LAST_ERROR" -ne "1" ] 
 then
-  echo -e "The detection of type error does not work properly"
-  ERROR_NB=1
+  echo -e "/!\ The detection of type error does not work properly"
+  ERROR=1
 fi
 
-../src/interval -blabla ../tests/test3 2>/dev/null
+echo -e "azeref \n efsef ef\n er" 1>data
+interval -blabla data 2>/dev/null
 LAST_ERROR=$?
 if [ "$LAST_ERROR" -ne "2" ] 
 then
-  echo -e "The detection of option's error does not work properly"
-  ERROR_NB=2
+  echo -e "/!\ The detection of option's error does not work properly"
+  ERROR=2
 fi
 
 
-if [ "$ERROR_NB" -eq "0" ] 
+if [ -e /usr/bin/valgrind ]
 then
-  echo -e "All tests on interval went well"
+  echo `seq 1 100` 1> data
+  valgrind interval data &>temp
+  grep '\(leaks\|alloc\)' temp >> finalmsg 
+  rm temp
+fi
+
+echo `seq 1 100000` 1> data2
+/usr/bin/time -a -o ./finalmsg -f "time taken for 100000 numbers : %e seconds\nused memory : %K" interval data2 >/dev/null 
+
+cat finalmsg
+if [ "$ERROR" -eq "0" ] 
+then
+  echo -e "\nAll tests on interval went well\n"
 fi
 
 
-exit $ERROR_NB
+exit
