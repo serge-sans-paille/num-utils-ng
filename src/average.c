ffe3d31c5c5819fd6c806027de2e8bd3cc55bf72
edern <edern_hotte@hotmail.fr>
2011-02-17 18:55:45 +0100

    the options "-i" and "-I" have been implemented and comments have been added.

--- src/average.c
+++ src/average.c
@@ -1,23 +1,37 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <unistd.h>
 
 double mean(FILE*);
 int fileIsEmpty(FILE*);
 int typeIsWrong(FILE*);
+int fileTest(FILE*);
 
-int typeIsWrong(FILE* stream){
+int fileTest(FILE* stream){				//this function triggers every tests on the file.
+    if (fileIsEmpty(stream)){
+      fprintf(stderr,"The file is empty \n");
+      return 1;	
+    }
+    if (typeIsWrong(stream)){
+      fprintf(stderr,"the type of the file is wrong \n");
+      return 1;
+    }
+    return 0;
+}
+
+int typeIsWrong(FILE* stream){				//this function tests if there is letters in the file.
   char d;
   while(!feof(stream)){
     fscanf(stream, "%c",&d);
     if ((d>57) || ((d<48) && (d>32) && (d!=46)))  
-  return 1;
+      return 1;
   }
   rewind(stream);
   return 0;
 }
 
 
-int fileIsEmpty(FILE* stream){
+int fileIsEmpty(FILE* stream){				//this function tests if the file is empty.
   long pos;
   fseek(stream, 0L, SEEK_END);
   pos=ftell(stream);
@@ -29,7 +43,7 @@ int fileIsEmpty(FILE* stream){
 }
 
 
-double mean(FILE *stream){
+double mean(FILE *stream){				//this function calculates the average from a File or stdin depending on the argument.
   double l=0;
   double moyenne=0;
   double chiffre=0;
@@ -39,25 +53,59 @@ double mean(FILE *stream){
     l++;
   }
   moyenne/=(l-1);
+  rewind(stream);
   return moyenne;
 }
 
 int main(int argc,char *argv[]){
-  FILE* tab = NULL;   
-  if (argc > 1){
-    tab = fopen(argv[1], "r");
-    if (fileIsEmpty(tab)){
-      fprintf(stderr,"The file is empty \n");
-      return 0;	
-    }
-    if (typeIsWrong(tab)){
-      fprintf(stderr,"the type of the file is wrong \n");
+  FILE* tab = NULL; 
+  int opt;
+  double d;                              //d is used to store any double which needs storage.
+  while((opt=getopt(argc,argv,"iI"))!=-1){
+    switch(opt) {
+
+    case 'i': 				// option "-i" (integer portion of the average)
+      if (argv[optind]!=NULL){
+        tab = fopen(argv[optind], "r");
+        if (fileTest(tab))
+          return 0;
+        fprintf(stderr,"The integer portion of the average number is %d \n",(int) mean(tab));
+        fclose(tab);
+        }
+      else 
+        printf("The integer portion of the average number is %d \n",(int) mean(stdin));
       return 0;
+      break;
+
+    case 'I': 				//option "-I" (decimal portion of the average)
+      if (argv[optind]!=NULL){
+        tab = fopen(argv[optind], "r");
+        if (fileTest(tab))
+          return 0;
+        fprintf(stderr,"The decimal portion of the average number is %lf \n",mean(tab) - (int) mean(tab));
+        fclose(tab);
+        }
+      else {
+        d=mean(stdin);
+        printf("The decimal portion of the average number is %lf \n",d - (int) d);
+      }
+      return 0;
+      break;
+
+    case '?':				//option fail.
+      printf("optionfail\n");
+      return 0;
+      break;
     }
-    fprintf(stderr,"The average number is %lf \n",mean(tab));
-    fclose(tab);
   }
-  else 
-    printf("The average number is %lf \n",mean(stdin));
+    if (argv[optind]!=NULL){		//normal use of "average".
+      tab = fopen(argv[optind], "r");
+      if (fileTest(tab))
+        return 0;
+      fprintf(stderr,"The average number is %lf \n",mean(tab));
+      fclose(tab);
+      }
+    else 
+      printf("The average number is %lf \n",mean(stdin));
   return 0;
 }
