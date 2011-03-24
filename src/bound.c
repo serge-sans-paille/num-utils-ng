f01cc570aca8eb19c4b08d2786a86327dee0ba75
flavien <flaven.moullec@telecom-bretagne.eu>
2011-03-24 14:26:26 +0100

    The license GNU GPLv3 has been added

--- src/bound.c
+++ src/bound.c
@@ -1,3 +1,35 @@
+/**
+*
+*
+* ***** BEGIN GPL LICENSE BLOCK *****
+*
+* This file is part of num-utils-nv project
+*
+* This program is free software; you can redistribute it and/or
+* modify it under the terms of the GNU General Public License
+* as published by the Free Software Foundation; either version 3
+* of the License, or (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program If not, see <http://www.gnu.org/licenses/>.
+*
+* The Original Code is Copyright (C) 2011 by num-utils-nv project.
+* All rights reserved.
+*
+* The Original Code is: all of this file.
+*
+* Contributor(s): none yet.
+*
+* ***** END GPL LICENSE BLOCK *****
+*/
+
+
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
