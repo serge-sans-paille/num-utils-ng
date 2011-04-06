799999a8a1d4dabd100cb44b654d4762cae2ba30
edern <edern_hotte@hotmail.fr>
2011-04-06 11:44:48 +0200

    Some minor mistakes related to error messages have been corrected.

--- src/round.c
+++ src/round.c
@@ -1,17 +1,116 @@
+/**
+*
+*
+* ***** BEGIN GPL LICENSE BLOCK *****
+*
+* This file is part of num-utils-nv project
+*
+* This program is free software; you can redistribute it and/or
+* modify it under the terms of the GNU General Public License
+* as published by the Free Software Foundation; either version 3
+* of the License, or (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program If not, see <http://www.gnu.org/licenses/>.
+*
+* The Original Code is Copyright (C) 2011 by num-utils-nv project.
+* All rights reserved.
+*
+* The Original Code is: all of this file.
+*
+* Contributor(s): none yet.
+*
+* ***** END GPL LICENSE BLOCK *****
+*/
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
 #include <math.h>
 
-int fileIsEmpty(FILE*);
-int typeIsWrong(FILE*);
-double decimalPortion(double);
-int roundc(FILE*,int,int);
+enum {
+	ERROR_1=1,
+	ERROR_2,
+     };
+
+
+double decimalPortion(double d){
+  int i;
+  double res;
+  i= (int) d;
+  res= d- (double) i;
+  return res;
+}
+
+int roundc(FILE* stream,int m,int n){
+  double d;
+  if (m==0){
+    while(fscanf(stream, "%lf",&d)!=EOF){     
+      if (fabs(decimalPortion(d))<0.5)
+        printf("result : %d\n", (int) d);
+      else{
+        if (d>=0)
+          printf("result : %d\n", (int) d+1);
+        else
+          printf("result : %d\n", (int) d-1);
+      }    
+    }
+  }
+  if (m==1){
+    while(fscanf(stream, "%lf",&d)!=EOF){
+      if (d==(int)d)
+        printf("result : %d\n", (int)d);
+      else{
+        if (d>=0)
+          printf("result : %d\n", (int) d+1);
+        else
+          printf("result : %d\n", (int) d);
+      }    
+    }
+  }
+  if (m==2){
+    while(fscanf(stream, "%lf",&d)!=EOF){
+      if (d>=0)
+        printf("result : %d\n", (int) d); 
+      else
+        printf("result : %d\n", (int) d-1);        
+    }
+  }
+  if (m==3){
+    while(fscanf(stream, "%lf",&d)!=EOF){     
+      d=d/(double)n;
+      if (fabs(decimalPortion(d))<0.5)
+        printf("result : %d\n", (int)d*n);
+      else
+        if (d>=0)
+          printf("result : %d\n", (int) (d+1)*n);
+        else
+          printf("result : %d\n", (int) (d-1)*n);            
+    }
+  }
+  return 0;
+}
+
+int typeIsWrong(FILE* stream){				//this function tests if there is letters in the file.
+  char d;
+  while(!feof(stream)){
+    fscanf(stream, "%c",&d);
+    if ((d>57) || ((d<48) && (d>32) && (d!=46))) { 
+      fprintf(stderr,"The type of the file is wrong\n");
+      return 1;
+    }
+  }
+  rewind(stream);
+  return 0;
+}
 
 int main(int argc,char *argv[]){
-  int opt;
-  int m=0;
-  int f=0;
+  int opt,m=0,f;
   FILE* stream=stdin;
     while((opt=getopt(argc,argv,"cfn:"))!=-1){
       switch(opt) {
@@ -31,122 +130,26 @@ int main(int argc,char *argv[]){
 
       default :				//option fail.
         fprintf(stderr,"invalid option \n");
-        return 3;
+        return ERROR_2;
       break;
       }
   }
 
   if (argc>optind){
-    stream = fopen(argv[optind], "r");
-    if (stream==NULL){
-      fprintf(stderr,"the file can't be opened, see \"errno\" for more infromation");
-      return 4;
+    if (!(stream = fopen(argv[optind], "r"))){
+      perror("memory allocation"); 
+      exit(EXIT_FAILURE);
     }
     if (typeIsWrong(stream))
-      return 2;
-    if (fileIsEmpty(stream))
-      return 1;
+      return ERROR_1;
   }   
-  
-
   roundc(stream, m,f);
-
-
-  if (argc>1){
+  if (argc>optind){
     if (fclose(stream)!=0){
-      fprintf(stderr,"the file can't be closed, see \"errno\" for more infromation");
-      return 5;
+      perror("memory allocation"); 
+      exit(EXIT_FAILURE);      
     }
   }
 
-  return 0;
-}
-
-
-double decimalPortion(double d){
-  int i;
-  double res;
-  i= (int) d;
-  res= d- (double) i;
-  return res;
-}
-
-int roundc(FILE* stream,int m,int n){
-  double d;
-  fscanf(stream, "%lf",&d);
-  if (m==0){
-    while(!feof(stream)){     
-      if (fabs(decimalPortion(d))<0.5)
-        printf("result : %d\n", (int) d);
-      else{
-        if (d>=0)
-          printf("result : %d\n", (int) d+1);
-        else
-          printf("result : %d\n", (int) d-1);
-      }
-     fscanf(stream, "%lf",&d);      
-    }
-  }
-  if (m==1){
-    while(!feof(stream)){
-      if (d==(int)d)
-        printf("result : %d\n", (int)d);
-      else{
-        if (d>=0)
-          printf("result : %d\n", (int) d+1);
-        else
-          printf("result : %d\n", (int) d);
-      }
-      fscanf(stream, "%lf",&d);      
-    }
-  }
-  if (m==2){
-    while(!feof(stream)){
-      if (d>=0)
-        printf("result : %d\n", (int) d); 
-      else
-        printf("result : %d\n", (int) d-1);    
-      fscanf(stream, "%lf",&d);    
-    }
-  }
-  if (m==3){
-    while(!feof(stream)){     
-      d=d/(double)n;
-      if (fabs(decimalPortion(d))<0.5)
-        printf("result : %d\n", (int)d*n);
-      else
-        if (d>=0)
-          printf("result : %d\n", (int) (d+1)*n);
-        else
-          printf("result : %d\n", (int) (d-1)*n);
-     fscanf(stream, "%lf",&d);            
-    }
-  }
-  return 0;
-}
-
-int fileIsEmpty(FILE* stream){				//this function tests if the file is empty.
-  long pos;
-  fseek(stream, 0L, SEEK_END);
-  pos=ftell(stream);
-  rewind(stream);
-  if (pos==0){
-    fprintf(stderr,"The file is empty\n");
-    return 1;
-  }
-  else 
-    return 0;
-}
-
-int typeIsWrong(FILE* stream){				//this function tests if there is letters in the file.
-  char d;
-  while(!feof(stream)){
-    fscanf(stream, "%c",&d);
-    if ((d>57) || ((d<48) && (d>32) && (d!=46))) { 
-      fprintf(stderr,"The type of the file is wrong\n");
-      return 1;
-    }
-  }
-  rewind(stream);
   return 0;
 }
