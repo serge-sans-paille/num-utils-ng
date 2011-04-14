189799372c9bafb3966119ed40362392e21a869a
flavien <flaven.moullec@telecom-bretagne.eu>
2011-04-14 23:05:39 +0200

    Errors have been deleted

--- src/numsum.c
+++ src/numsum.c
@@ -34,82 +34,54 @@
 #include <unistd.h>
 
 enum {ERROR_2};
-double sum (FILE*);
-int fileIsEmpty(FILE*);
-int typeIsWrong(FILE*);
-	
 
 double sum ( FILE* fichier){		//this function calculates the sum of numbers from a file or stdin.
-	double sum=0.;
-	double number=0.;
-	while (!feof(fichier)){
-		sum = sum + number ;					
-		fscanf (fichier,"%lf",&number);
-	}
-return sum;
+  double sum=0.;
+  double number=0.;
+  while (!feof(fichier)){
+    sum = sum + number ;					
+    fscanf (fichier,"%lf",&number);
+  }
+ return sum;
 }
 
-int fileTest(FILE* fichier){		//this function triggers every tests on the file.
-    if (fileIsEmpty(fichier)){
-      fprintf(stderr,"The file is empty \n");
-      return 1;	
-    }
-    if (typeIsWrong(fichier)){
-      fprintf(stderr,"the type of the file is wrong \n");
-      return 1;
-    }
-    return 0;
-}
-
-
-int fileIsEmpty(FILE*fichier){			//this function tests if the file is empty.
-  long position;
-  fseek(fichier, 0L, SEEK_END);
-  position=ftell(fichier);
-  rewind(fichier);
-  if (position==0)
-    return 1;
-  else 
-    return 0;
-}
 
 int typeIsWrong(FILE*fichier){			//this function tests if there is letters in the file.	
-	char c ;
-	while (!feof(fichier)){
-		fscanf ( fichier, "%c", &c );
-		if ( (c>57) || ((c<48) && (c>32) && (c!=46)))
-			return 1 ;
-	}
-	rewind (fichier);
-	return 0;
+  char c ;
+    while (!feof(fichier)){
+      fscanf ( fichier, "%c", &c );
+      if ( (c>57) || ((c<48) && (c>32) && (c!=46)))
+        return 1 ;
+      }
+  rewind (fichier);
+  return 0;
 }
 
 int main(int argc,char *argv[]){
-FILE* file = NULL;
-int opt;
-double d;
-double r;
+  FILE* file = NULL;
+  int opt;
+  double d,r;
 	
-while((opt = getopt(argc,argv,"iIcrh"))!=-1){
-	switch(opt) {
+  while((opt = getopt(argc,argv,"iIcrh"))!=-1){
+    switch(opt) {
 
-	case 'i':			// option "-i" (integer portion of the final sum)
-	if (argv[optind]!=NULL){
-        	file = fopen(argv[optind], "r");
-        	if (fileTest(file))
-          	return 0;
-        	fprintf(stderr,"The integer portion of the sum is %d \n",(int)sum(file));
-        	fclose(file);
-        	}
+      case 'i':			// option "-i" (integer portion of the final sum)
+        if (argv[optind]!=NULL){
+          file = fopen(argv[optind], "r");
+          if (typeIsWrong(file))
+            return 1;
+          fprintf(stderr,"The integer portion of the sum is %d \n",(int)sum(file));
+          fclose(file);
+        }
 	else 
-        printf("The integer portion of the sum is %d \n",(int)sum(stdin));
-	return 0;
+          printf("The integer portion of the sum is %d \n",(int)sum(stdin));
+	  return 0;
 	break;
 
-	case 'I':			//option "-I" (decimal portion of the final sum)
-	if (argv[optind]!=NULL){
-        	file = fopen(argv[optind], "r");
-        	if (fileTest(file))
+      case 'I':			//option "-I" (decimal portion of the final sum)
+        if (argv[optind]!=NULL){
+          file = fopen(argv[optind], "r");
+            if (fileTest(file))
           		return 0;
 			r = (int)sum(file);		 
 			d = sum(file)-r; 
@@ -122,72 +94,72 @@ while((opt = getopt(argc,argv,"iIcrh"))!=-1){
 	return 0;
 	break;
 
-	case 'c':			//option "-c" (Print out the sum of each column.)
+      case 'c':			//option "-c" (Print out the sum of each column.)
 	if (argv[optind]!=NULL){
-        	file = fopen(argv[optind], "r");
-        	if (fileTest(file))
-          	return 0;
-			int l=0,i,j,I,J;
-			int C=0;
-			int car; 
-			if ( file != NULL){
-				while ( (car= getc(file)) != EOF){
-					if (car == '\n')
-					++l;	
-					if (car == ' ' && l==0)
-					++C;
-				}
-			int tableau[l][C+1];
-			int* tab=NULL;
-			tab=malloc(C*sizeof(int));
-			rewind(file);
-			for(i=0; i<l*(C+1); i++){
-			fscanf(file,"%d",&tableau[i/(C+1)][i % (C+1)]);
-			}
-			for (J=0;J<C+1;J++){
-				for (I=0;I<l;I++){
-					tab[J]=tab[J]+tableau[I][J];
-				}
-			printf("The sum of column %d is %d \n",J+1,tab[J]);
-			fclose(file);
-	}
-	return 0;
+          file = fopen(argv[optind], "r");
+          if (typeIsWrong(file))
+            return 1;
+          int l=0,i,I,J;
+          int C=0;
+          int car; 
+	  if (file != NULL){
+	    while ( (car= getc(file)) != EOF){
+	      if (car == '\n')
+	        ++l;	
+	      if (car == ' ' && l==0)
+		++C;
+	    }
+	    int tableau[l][C+1];
+	    int* tab=NULL;
+	    tab=malloc(C*sizeof(int));
+	    rewind(file);
+	    for(i=0; i<l*(C+1); i++){
+	      fscanf(file,"%d",&tableau[i/(C+1)][i % (C+1)]);
+	    }
+	    for (J=0;J<C+1;J++){
+	      for (I=0;I<l;I++){
+	        tab[J]=tab[J]+tableau[I][J];
+	      }
+	      printf("The sum of column %d is %d \n",J+1,tab[J]);
+	    }
+	    fclose(file);
+	  }
+	  return 0;
+        }
 	break;
 	
-	case 'r':			//option "-r" (Print out the sum of each row.)
-	if (rgv[optind]!=NULL){
-        	file = fopen(argv[optind], "r");
-        	if (fileTest(file))
-          	return 0;
+      case 'r':			//option "-r" (Print out the sum of each row.)
+	if (argv[optind]!=NULL){
+          file = fopen(argv[optind], "r");
+          if (typeIsWrong(file))
+            return 1;
+	}
+      break;
 
 			
-	case 'h':
+      case 'h':
 	printf ( " sorry, the help page is not yet available.\n");
 	return 0;
-	break;
+      break;
 					
-	default :				//option fail.
+      default :				//option fail.
         fprintf(stderr, "Invalid option\n");
         return ERROR_2;
       break;
 	
-	}
-	}
+    }
+  }
 	
-	if (argv[optind]!=NULL){		
-		file=fopen(argv[optind], "r");		
-		if (fileTest(file))			
-			return 0;
-		fprintf(stderr,"The sum is %lf  \n",sum(file));
-		fclose(file);
-	}
-	else
-		printf("The sum is %lf \n",sum(stdin));
-	return 0;
-
-
-
-
+  if (argv[optind]!=NULL){		
+    file=fopen(argv[optind], "r");		
+    if (typeIsWrong(file))			
+      return 1;
+    fprintf(stderr,"The sum is %lf  \n",sum(file));
+    fclose(file);
+  }
+  else
+    printf("The sum is %lf \n",sum(stdin));
+  return 0;
 }
 	
 
