22093bb8b2c3921216615f033a2dbc560f766623
edern <edern_hotte@hotmail.fr>
2011-05-04 20:56:38 +0200

    Some tests have been added.

--- tests/testnumprocess.sh
+++ tests/testnumprocess.sh
@@ -2,44 +2,97 @@
 
 #This script should gather all the tests for the function numprocess.
 
-ERROR_NB=0
+ERROR=0
 LAST_ERROR=0
 
-../src/numprocess /+1/ ../tests/test3 2>/dev/null
+echo -e "===========================================================================\nTest results for the numprocess function :\n" > finalmsg
+
+echo -e "azeref \n efsef ef\n er" 1>data
+numprocess /+1/ data 2>/dev/null
 LAST_ERROR=$?
 if [ "$LAST_ERROR" -ne "1" ] 
 then
-  echo -e "The detection of type error does not work properly"
-  ERROR_NB=1
+  echo -e "/!\ The detection of type error does not work properly"
+  ERROR=1
 fi
 
-../src/numprocess -blabla ../tests/test3 2>/dev/null
+echo "1 2 3 4" >data
+numprocess -blabla data 2>/dev/null
 LAST_ERROR=$?
 if [ "$LAST_ERROR" -ne "2" ] 
 then
-  echo -e "The detection of option failure does not work properly"
-  ERROR_NB=2
+  echo -e "/!\ The detection of option failure does not work properly"
+  ERROR=2
 fi
 
-../src/numprocess 2>/dev/null
+numprocess 2>/dev/null
 LAST_ERROR=$?
 if [ "$LAST_ERROR" -ne "3" ] 
 then
-  echo -e "The detection of missing expression does not work properly"
-  ERROR_NB=3
+  echo -e "/!\ The detection of missing expression does not work properly"
+  ERROR=3
 fi
 
-../src/numprocess rge ../tests/test3 2>/dev/null
+numprocess rge data 2>/dev/null
 LAST_ERROR=$?
 if [ "$LAST_ERROR" -ne "4" ] 
 then
-  echo -e "The detection of wrong expressions does not work properly"
-  ERROR_NB=4
+  echo -e "/!\ The detection of wrong expressions does not work properly"
+  ERROR=4
 fi
 
-if [ "$ERROR_NB" -eq "0" ] 
+echo -e "\t 1.Addition :" >> finalmsg
+
+if [ -e /usr/bin/valgrind ]
 then
-  echo -e "All tests on numprocess went well"
+  echo `seq 1 100` 1> data
+  valgrind numprocess /+1/ data &>tempo
+  grep '\(leaks\|alloc\)' tempo >> finalmsg 
+  rm tempo
 fi
 
-exit $ERROR_NB
+echo `seq 1 10000` 1> data2
+/usr/bin/time -a -o ./finalmsg -f "time taken for 10000 numbers : %e seconds\nused memory : %K" numprocess /+1/ data2 >/dev/null 
+
+echo -e "\t 2.Substraction :" >> finalmsg
+
+if [ -e /usr/bin/valgrind ]
+then
+  valgrind numprocess /-1/ data &>tempo
+  grep '\(leaks\|alloc\)' tempo >> finalmsg 
+  rm tempo
+fi
+
+/usr/bin/time -a -o ./finalmsg -f "time taken for 10000 numbers : %e seconds\nused memory : %K" numprocess /-1/ data2 >/dev/null 
+
+echo -e "\t 3.Multiplication :" >> finalmsg
+
+if [ -e /usr/bin/valgrind ]
+then
+  valgrind numprocess /*2/ data &>tempo
+  grep '\(leaks\|alloc\)' tempo>> finalmsg 
+  rm tempo
+fi
+
+/usr/bin/time -a -o ./finalmsg -f "time taken for 10000 numbers : %e seconds\nused memory : %K" numprocess /*2/ data2 >/dev/null 
+
+echo -e "\t 4.Division :" >> finalmsg
+
+if [ -e /usr/bin/valgrind ]
+then
+  valgrind numprocess /%2/ data &>tempo
+  grep '\(leaks\|alloc\)' tempo>> finalmsg 
+  rm tempo
+fi
+
+/usr/bin/time -a -o ./finalmsg -f "time taken for 10000 numbers : %e seconds\nused memory : %K" numprocess /%2/ data2 >/dev/null 
+if [ "$ERROR" -eq "0" ] 
+then
+  echo -e "\nAll tests on numprocess went well\n" >> finalmsg
+fi
+
+cat finalmsg
+rm finalmsg
+rm data
+rm data2
+exit $ERROR
